beb1898e5ece2cc1bf73efec7dae9f39186e7730
Shahtaz <shanto@echologyx.com>
2025-05-22 17:16:39 +0600

    added exerience to admin

--- services/admin.py
+++ services/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from services.models import Services, Skills
+from services.models import Services, Skills, Experience
 
 @admin.register(Services)
 class ServicesAdmin(admin.ModelAdmin):
@@ -13,3 +13,9 @@ class SkillsAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description']
     ordering = ['-order']
     list_display = ("name", "proficiency_level", "order", "created_at")
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    search_fields = ['position', 'company_name']
+    ordering = ['-started_at']
+    list_display = ("position", "company_name", "company_location", "started_at")
