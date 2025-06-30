d0ab7631d966a2b274537d8c7d0d584285a56ed1
Shahtaz <shanto@echologyx.com>
2025-06-30 22:04:03 +0600

    visitor stats added

--- base/models.py
+++ base/models.py
@@ -61,6 +61,7 @@ class Client(models.Model):
     budget = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     timeline = models.CharField(max_length=64, blank=True, null=True)
     design_required = models.BooleanField(default=False)
+    is_onboarded = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     
