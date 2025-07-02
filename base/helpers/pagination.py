6188f63519d89abc070f01729750a5945ef61099
Shahtaz Rahman <shahtazrahman17@gmail.com>
2025-07-03 03:52:58 +0600

    user in conversation

--- base/helpers/pagination.py
+++ base/helpers/pagination.py
@@ -1,4 +1,4 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 class CustomPagination(PageNumberPagination):
     """
@@ -7,3 +7,8 @@ class CustomPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+class DynamicPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
